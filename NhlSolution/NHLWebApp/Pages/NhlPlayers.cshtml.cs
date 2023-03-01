810799d06670cbcfe9707877f005e2b8232c509a
kiana <112987031+kianaleslie@users.noreply.github.com>
2023-03-01 14:39:11 -0700

    added new razor page but need to catch up on json file in web app

---
+++ NhlSolution/NHLWebApp/Pages/NhlPlayers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NHLWebApp.Pages
+{
+    public class NhlPlayersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
